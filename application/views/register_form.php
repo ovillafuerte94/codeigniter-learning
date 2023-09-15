5819edbaf950fe880ad3b5c2d77315f3865c6bb2
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-15 02:47:36 -0400

    Error message on register page

--- application/views/register_form.php
+++ application/views/register_form.php
@@ -7,14 +7,19 @@
 
 <body>
     <h2>Register</h2>
+    <?php if ($this->session->flashdata('error')) : ?>
+        <ul>
+            <li><?php echo $this->session->flashdata('error'); ?></li>
+        </ul>
+    <?php endif; ?>
     <?php echo form_open('register'); ?>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br>
-        <label for="username">E-mail:</label>
-        <input type="email" id="email" name="email" required><br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br>
-        <input type="submit" value="Register ">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required><br>
+    <label for="username">E-mail:</label>
+    <input type="email" id="email" name="email" required><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br>
+    <input type="submit" value="Register ">
     <?php echo form_close(); ?>
 </body>
 
