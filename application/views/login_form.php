827adf3fa4c9b9158ef9e9f3818ed11d413c019b
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-15 02:47:29 -0400

    Error message on login page

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -7,12 +7,17 @@
 
 <body>
     <h2>Login</h2>
+    <?php if ($this->session->flashdata('error')) : ?>
+        <ul>
+            <li><?php echo $this->session->flashdata('error'); ?></li>
+        </ul>
+    <?php endif; ?>
     <?php echo form_open('login'); ?>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br>
-        <input type="submit" value="Login">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br>
+    <input type="submit" value="Login">
     <?php echo form_close(); ?>
 </body>
 
