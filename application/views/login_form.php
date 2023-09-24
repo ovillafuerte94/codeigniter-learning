00c09d4a187607afb155616aec64dcda18ca9bf2
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-24 19:54:53 -0400

    Bootstrap implementation, Interface redesign
    - Added Bootstrap 5 for design.
    - Redesigned login and registration forms.
    - Implemented dynamic logic in Navbar to show 'Logout' when user is logged in, or 'Login' and 'Register' if not.
    - Customized home page styles and layout."

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,24 +1,33 @@
-<!DOCTYPE html>
-<html>
+<?php $this->load->view('layouts/header.php'); ?>
 
-<head>
-    <title>Login</title>
-</head>
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title text-center">Login</h2>
 
-<body>
-    <h2>Login</h2>
-    <?php if ($this->session->flashdata('error')) : ?>
-        <ul>
-            <li><?php echo $this->session->flashdata('error'); ?></li>
-        </ul>
-    <?php endif; ?>
-    <?php echo form_open('login'); ?>
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required><br>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required><br>
-    <input type="submit" value="Login">
-    <?php echo form_close(); ?>
-</body>
+                <?php if ($this->session->flashdata('error')) : ?>
+                    <div class="alert alert-danger">
+                        <?php echo $this->session->flashdata('error'); ?>
+                    </div>
+                <?php endif; ?>
 
-</html>
+                <?php echo form_open('login'); ?>
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="username" class="form-control" id="username" name="username" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <div class="text-end">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </div>
+                <?php echo form_close(); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php $this->load->view('layouts/footer.php'); ?>
--- application/views/register_form.php
+++ application/views/register_form.php
@@ -1,26 +1,39 @@
-<!DOCTYPE html>
-<html>
+<?php $this->load->view('layouts/header'); ?>
 
-<head>
-    <title>Register</title>
-</head>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title text-center">Register</h2>
 
-<body>
-    <h2>Register</h2>
-    <?php if ($this->session->flashdata('error')) : ?>
-        <ul>
-            <li><?php echo $this->session->flashdata('error'); ?></li>
-        </ul>
-    <?php endif; ?>
-    <?php echo form_open('register'); ?>
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required><br>
-    <label for="username">E-mail:</label>
-    <input type="email" id="email" name="email" required><br>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required><br>
-    <input type="submit" value="Register ">
-    <?php echo form_close(); ?>
-</body>
+                    <?php if ($this->session->flashdata('error')) : ?>
+                        <div class="alert alert-danger">
+                            <?php echo $this->session->flashdata('error'); ?>
+                        </div>
+                    <?php endif; ?>
 
-</html>
+                    <form action="<?php echo base_url('register'); ?>" method="post">
+                        <div class="mb-3">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="username" class="form-control" id="username" name="username" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">E-mail</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+                        <div class="text-end">
+                            <button type="submit" class="btn btn-success">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php $this->load->view('layouts/footer'); ?>
